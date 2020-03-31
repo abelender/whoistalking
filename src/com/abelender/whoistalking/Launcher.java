85d147ba02596c16bd9f12b57c7a5c165aa20bc4
abelender <jonuel.martiguez@gmail.com>
2020-03-31 21:52:21 +0200

    Create Launcher file

--- src/com/abelender/whoistalking/Launcher.java
+++ src/com/abelender/whoistalking/Launcher.java
@@ -1,4 +1,28 @@
 package com.abelender.whoistalking;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Launcher {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Who Is Talking");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+
+        JPanel panel = new JPanel(new FlowLayout());
+
+        JButton buttonStart = new JButton("Iniciar");
+        JButton buttonStop = new JButton("Detener");
+        JButton buttonClean = new JButton("Limpiar");
+        JTextArea output = new JTextArea(10, 30);
+            output.setEditable(false);
+
+        panel.add(buttonStart);
+        panel.add(buttonStop);
+        panel.add(buttonClean);
+        panel.add(output);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
 }
